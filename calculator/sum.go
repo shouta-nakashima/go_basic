ee15dd3564904fc0b70867aaec7fc5d6ee07c43f
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2024-04-09 21:07:29 +0900

    feet: module and packages

---
+++ calculator/sum.go
@@ -0,0 +1,8 @@
+package calculator
+
+var offset float64 = 1
+var Offset float64 = 1
+
+func Sum(a float64, b float64) float64 {
+	return a + b + offset
+}
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go_basic/calculator"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// NOTE: hello world is commented out to avoid the "no output" error
+	//fmt.Println("Hello, World!")
+	err := godotenv.Load()
+	if err != nil {
+		return
+	}
+	fmt.Println(os.Getenv("GO_ENV"))
+	fmt.Println(calculator.Offset)
+	fmt.Println(calculator.Sum(1, 2))
+	fmt.Println(calculator.Multiply(1, 2))
 }
